48dd1713e27466b12d5b9548e8c78a18e4cefe0e
Cleo Menezes <produtor.junior.menezes@gmail.com>
2020-11-13 01:07:44 -0300

    working

--- main.py
+++ main.py
@@ -22,28 +22,66 @@ class Handler(object):
         self.install_lampp = builder.get_object('install_lampp')
         self.button_a_restart = builder.get_object('button_a_restart')
         self.button_m_restart = builder.get_object('button_m_restart')
+        self.button_start_all = builder.get_object('button_start_all')
+        self.button_stop_all = builder.get_object('button_stop_all')
+        self.button_restart_all = builder.get_object('button_restart_all')
+        self.button_p_start = builder.get_object('button_p_start')
+        self.button_p_stop = builder.get_object('button_p_stop')
+        self.proftp_status = builder.get_object('proftp_status')
+        self.button_p_stop = builder.get_object('button_p_stop')
+        self.button_p_restart = builder.get_object('button_p_restart')
 
-###########################################################################3
 
-#putting current status of apacher service
+
+###########################################################################
+
+#putting current status of apacher service WORKING
         status_command_a  = os.popen('service apache2 status').readlines()
-
         a_first_status = self.apache_status.get_label()
-        if str(a_first_status) != 'Active' or str(a_first_status) != 'Inactive':
-            if 'dead' in str(status_command_a[2]):
-                self.apache_status.set_text('Inactive')
-            elif 'running' in str(status_command_a[2]):
-                self.apache_status.set_text('Active')
+        try:
+            if str(a_first_status) != 'Active' or str(a_first_status) != 'Inactive':
+                if 'dead' in str(status_command_a[2]):
+                    self.apache_status.set_text('Inactive')
+                elif 'running' in str(status_command_a[2]):
+                    self.apache_status.set_text('Active')
+        except:
+            if str(a_first_status) != 'Active' or str(a_first_status) != 'Inactive':
+                status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+                if 'Apache is not running' in str(status_command[1]):
+                    self.apache_status.set_text('Inactive')
+                elif 'Apache is running' in str(status_command[1]):
+                    self.apache_status.set_text('Active')
+
+
+
 #putting current status of mysql service
         
         status_command_m  = os.popen('service mysql status').readlines()
-
         m_first_status = self.mysql_status.get_label()
-        if str(m_first_status) != 'Active' or str(m_first_status) != 'Inactive':
-            if 'dead' in str(status_command_m[2]):
-                self.mysql_status.set_text('Inactive')
-            elif 'running' in str(status_command_m[2]):
-                self.mysql_status.set_text('Active')
+        try:
+            if str(m_first_status) != 'Active' or str(m_first_status) != 'Inactive':
+                if 'dead' in str(status_command_m[2]):
+                    self.mysql_status.set_text('Inactive')
+                elif 'running' in str(status_command_m[2]):
+                    self.mysql_status.set_text('Active')
+        except:
+            if str(a_first_status) != 'Active' or str(a_first_status) != 'Inactive':
+                status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+                if 'MySQL is not running' in str(status_command[2]):
+                    self.mysql_status.set_text('Inactive')
+                elif 'MySQL is running' in str(status_command[2]):
+                    self.mysql_status.set_text('Active')
+
+
+#putting current status of proftp service
+
+        p_first_status = self.proftp_status.get_label()
+        if str(p_first_status) != 'Active' or str(p_first_status) != 'Inactive':
+            status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+            if 'ProFTPD is deactivated' in str(status_command[3]):
+                self.proftp_status.set_text('Inactive')
+            elif 'ProFTPD is running' in str(status_command[3]):
+                self.proftp_status.set_text('Active')
 
 ################################################################################
 
@@ -54,67 +92,208 @@ class Handler(object):
         Gtk.main_quit()
 
 
-#apache start button
+################################### APACHE BUTTONS ##############################################
+
+#apache start button WORKING
     def on_button_a_start_clicked(self, *args):
-        os.system('service apache2 start')
-        status_command  = os.popen('service apache2 status').readlines()
-        if 'dead' in str(status_command[2]):
-            self.apache_status.set_text('Inactive')
-        elif 'running' in str(status_command[2]):
-            self.apache_status.set_text('Active')
-
-
-#apache stop button
-    def on_button_a_stop_clicked(self, *args):
-        os.system('service apache2 stop')
-        status_command  = os.popen('service apache2 status').readlines()
-
-        if 'running' in str(status_command[2]):
-            self.apache_status.set_text('Active')
-        elif 'dead' in str(status_command[2]):
-            self.apache_status.set_text('Inactive')
-
-
-#mysql start button
-    def on_button_m_start_clicked(self, *args):
-        os.system('service mysql start')
-        status_command  = os.popen('service mysql status').readlines()
-        if 'dead' in str(status_command[2]):
-            self.mysql_status.set_text('Inactive')
-        elif 'running' in str(status_command[2]):
-            self.mysql_status.set_text('Active')
-
-
-#mysql stop button
-    def on_button_m_stop_clicked(self, *args):
-        os.system('service mysql stop')
-        status_command  = os.popen('service mysql status').readlines()
-
-        if 'running' in str(status_command[2]):
-            self.mysql_status.set_text('Active')
-        elif 'dead' in str(status_command[2]):
-            self.mysql_status.set_text('Inactive')
-
-    
-#open directory button
-    def on_open_directory_clicked(self, *args):
-        import webbrowser
-        os.system('nautilus /var/www/html')
         
-    
-    
-#restart apache button
-    def on_button_a_restart_clicked(self, *args):
-        os.system('service apache2 restart')
+        try:
+            if 'Cannot run program' not in os.popen('service apache2 status').readlines()[0]:            
+                os.system('service apache2 start')
+                status_command = os.popen('service apache2 status').readlines()  
+                if 'dead' in str(status_command[2]):
+                    self.apache_status.set_text('Inactive')
+                elif 'running' in str(status_command[2]):
+                    self.apache_status.set_text('Active')
+        except:
+            os.popen('pkexec /opt/lampp/lampp startapache')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'Apache is not running' in str(status_command[1]):
+                self.apache_status.set_text('Inactive')
+            elif 'Apache is running' in str(status_command[1]):
+                self.apache_status.set_text('Active')
 
-        self.mysql_status.set_text('Active')
+
+#apache stop button WORKING
+    def on_button_a_stop_clicked(self, *args):
+        
+        try:
+            if 'Cannot run program' not in os.popen('service apache2 status').readlines()[0]:
+                os.system('service apache2 stop')
+                status_command  = os.popen('service apache2 status').readlines()
+                if 'running' in str(status_command[2]):
+                    self.apache_status.set_text('Active')
+                elif 'dead' in str(status_command[2]):
+                    self.apache_status.set_text('Inactive')
+        except:
+            os.popen('pkexec /opt/lampp/lampp stopapache')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'Apache is not running' in str(status_command[1]):
+                self.mysql_status.set_text('Inactive')
+            elif 'Apache is running' in str(status_command[1]):
+                self.apache_status.set_text('Active')
+
+
+#restart apache button WORKING
+    def on_button_a_restart_clicked(self, *args):
+        try:
+            if 'Cannot run program' not in os.popen('service apache2 status').readlines()[0]:
+
+                os.system('service apache2 restart')
+                status_command = os.popen('service apache2 status').readlines()
+                if 'running' in str(status_command[2]):
+                    self.apache_status.set_text('Active')
+                elif 'dead' in str(status_command[2]):
+                    self.apache_status.set_text('Inactive')
+        except:
+            os.popen('pkexec /opt/lampp/lampp reloadapache')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'Apache is not running' in str(status_command[1]):
+                self.apache_status.set_text('Inactive')
+            elif 'Apache is running' in str(status_command[1]):
+                self.apache_status.set_text('Active')
+
+
+########################################### MYSQSL BUTTONS ##################################################
+
+#mysql start button WORKING
+    def on_button_m_start_clicked(self, *args):
+
+        try:
+            if 'Cannot run program' not in os.popen('service mysql status').readlines()[0]:
+                os.system('service mysql start')
+                status_command  = os.popen('service mysql status').readlines()
+                if 'dead' in str(status_command[2]):
+                    self.mysql_status.set_text('Inactive')
+                elif 'running' in str(status_command[2]):
+                    self.mysql_status.set_text('Active')
+        except:
+            os.popen('pkexec /opt/lampp/lampp startmysql')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'MySQL is not running' in str(status_command[2]):
+                self.mysql_status.set_text('Inactive')
+            elif 'MySQL is running' in str(status_command[2]):
+                self.mysql_status.set_text('Active')
+
+
+
+
+#mysql stop button WORKING
+    def on_button_m_stop_clicked(self, *args):
+        try:
+            if 'Cannot run program' not in os.popen('service mysql status').readlines()[0]:
+                os.system('service mysql stop')
+                status_command  = os.popen('service mysql status').readlines()
+
+                if 'running' in str(status_command[2]):
+                    self.mysql_status.set_text('Active')
+                elif 'dead' in str(status_command[2]):
+                    self.mysql_status.set_text('Inactive')
+        except:
+            os.popen('pkexec /opt/lampp/lampp stopmysql')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'MySQL is not running' in str(status_command[2]):
+                self.mysql_status.set_text('Inactive')
+            elif 'MySQL is running' in str(status_command[2]):
+                self.mysql_status.set_text('Active')
 
             
 #restart mysql button
     def on_button_m_restart_clicked(self, *args):
-        os.system('service mysql restart')
+        try:
+            if 'Cannot run program' not in os.popen('service mysql status').readlines()[0]:
+                os.system('service mysql restart')
+                status_command = os.popen('service mysql status').readlines()
+                if 'running' in str(status_command[2]):
+                    self.mysql_status.set_text('Active')
+                elif 'dead' in str(status_command[2]):
+                    self.mysql_status.set_text('Inactive')
+        except:
+            os.popen('pkexec /opt/lampp/lampp reloadmysql')
+            status_command  = os.popen('opt/lampp/lampp status').readlines()
+            if 'MySQL is not running' in str(status_command[2]):
+                self.mysql_status.set_text('Inactive')
+            elif 'MySQL is running' in str(status_command[2]):
+                self.mysql_status.set_text('Active')
 
-        self.mysql_status.set_text('Active')
+########################################## ProFTPD BUTTON ################################################    
+
+#start ProFTPD button
+    def on_button_p_start_clicked(self, *args):
+
+        os.popen('pkexec /opt/lampp/lampp startftp')
+        status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+        if 'ProFTPD is deactivated' in str(status_command[3]):
+            self.proftp_status.set_text('Inactive')
+        elif 'ProFTPD is running' in str(status_command[3]):
+            self.proftp_status.set_text('Active')
+    
+#stop ProFTPD button    
+    def on_button_p_stop_clicked(self, *args):
+
+        os.popen('pkexec /opt/lampp/lampp stopftp')
+        status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+        if 'ProFTPD is deactivated' in str(status_command[3]):
+            self.proftp_status.set_text('Inactive')
+        elif 'ProFTPD is running' in str(status_command[3]):
+            self.proftp_status.set_text('Active')
+
+#restart ProFTPD button    
+    def on_button_p_restart_clicked(self, *args):
+
+        os.popen('pkexec /opt/lampp/lampp reloadftp')
+        status_command  = os.popen('pkexec /opt/lampp/lampp status').readlines()
+        if 'ProFTPD is deactivated' in str(status_command[3]):
+            self.proftp_status.set_text('Inactive')
+        elif 'ProFTPD is running' in str(status_command[3]):
+            self.proftp_status.set_text('Active')
+
+##########################################################################################################
+
+#open directory button
+    def on_open_directory_clicked(self, *args):
+        try:
+            os.system('nautilus /var/www/html')
+        except:
+            os.system('nemo /var/www/html')
+        else:
+            os.system('dolphin /var/www/html')
+
+# open about
+    
+
+
+#start all button
+    def on_button_start_all_clicked(self, *args):
+
+        try:
+            self.on_button_a_start_clicked()
+            self.on_button_m_start_clicked()
+            self.on_button_p_start_clicked()
+        except:
+            os.system('pkexec /opt/lampp/lampp start')
+
+    
+#stop all button
+    def on_button_stop_all_clicked(self, *args):
+        try:
+            self.on_button_a_stop_clicked()
+            self.on_button_m_stop_clicked()
+            self.on_button_p_stop_clicked()
+        except IndexError:
+            os.system('pkexec /opt/lampp/lampp stop')
+
+
+#restart all button
+    def on_button_restart_all_clicked(self, *args):
+        try:
+            self.on_button_a_restart_clicked()
+            self.on_button_m_restart_clicked()
+            self.on_button_p_restart_clicked()
+        except IndexError:
+            os.system('pkexec /opt/lampp/lampp restart')
+        
+
 
 
 
