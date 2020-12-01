c738137ca1aa032f17c074952d6243b5792c066e
Cleo Menezes Jr <produtor.junior.menezes@gmail.com>
2020-12-01 01:30:13 -0300

    fixing apache installation

--- install/install.sh
+++ install/install.sh
@@ -1,155 +1,103 @@
 #!/bin/bash
 
-echo '#################### Welcome LAMPP Manager install ####################';
-sleep 3;
+echo '#################### Welcome LAMPP Manager install ####################'
 echo ; echo ; echo
-echo '>> This script will install everything you need to have LAMPP on your computer.';
-sleep 3;
+echo '>> This script will install everything you need to have LAMPP on your computer.'
 sudo apt install wget
 echo ; echo ; echo
-echo '#################### Now lets install Apache2 ####################';
-sleep 3;
+echo '#################### Now lets install Apache2 ####################'
 echo ; echo ; echo
-sudo apt install apache2 -y;
+sudo apt install apache2 -y
 echo ; echo ; echo
-echo '>> Take a look if you installed correctly by accessing localhost.';
+echo '>> Take a look if you installed correctly by accessing localhost.'
 echo ; echo ; echo
-sleep 3;
-sensible-browser localhost;
-sleep 3;
+sensible-browser localhost
 echo ; echo ; echo
-echo '>> If the browser does not open correctly, access localhost through it.';
-sleep 3
+echo '>> If the browser does not open correctly, access localhost through it.'
 echo ; echo ; echo
-echo '#################### Now lets install MariaDB(MySQL) ####################';
+echo '#################### Now lets install MariaDB(MySQL) ####################'
 echo ; echo ; echo
-sleep 3;
-sudo apt install mariadb-server -y;
+sudo apt install mariadb-server -y
 echo ; echo ; echo
-echo '>> We will also install your respective client';
+echo '>> We will also install your respective client'
 echo ; echo ; echo
-sudo apt install mariadb-client -y;
+sudo apt install mariadb-client -y
 echo ; echo ; echo
-#echo '#################### Lets secure the MySQL installation ####################'
-#echo ; echo ; echo
-#sleep 4;
-#echo '>> It will ask us if we want to change the root password, if so, enter it 2 times.'
-#sleep 10;
-#echo ; echo ; echo
-#sudo mysql_secure_installation;
-
 echo '>> Remember that after installation you will still need to configure MySQL.'
-sleep 3;
 echo ; echo ; echo
-echo '#################### Now lets install PHP. ####################';
+echo '#################### Now lets install PHP. ####################'
 echo ; echo ; echo
-sleep 3;
-echo '>> We will also install php-fpm, php-gd, php-curl, php-mysql and libapache2-mod-php.';
+echo '>> We will also install php-fpm, php-gd, php-curl, php-mysql and libapache2-mod-php.'
 echo ; echo ; echo
-sleep 3;
-sudo apt install php php-fpm php-gd php-curl php-mysql libapache2-mod-php -y;
-sleep 3;
-echo 'Check if PHP has been installed correctly';
-sleep 2;
-wget https://github.com/CleoMenezes/LAMPP-Manager/blob/master/configuration%20files/info.php;
+sudo apt install php php-fpm php-gd php-curl php-mysql libapache2-mod-php -y
+echo 'Check if PHP has been installed correctly'
+wget https://github.com/CleoMenezes/LAMPP-Manager/blob/master/configuration%20files/info.php
 sudo mv info.php /var/www/html/
 sensible-browser http://localhost/info.php
 echo ; echo ; echo
-echo '>> I suppose you will also need optimized settings for Wordpress.';
-sleep 3;
+echo '>> I suppose you will also need optimized settings for Wordpress.'
 echo ; echo ; echo
-echo '#################### Lets activate the REWRITE module. ####################';
+echo '#################### Lets activate the REWRITE module. ####################'
 echo ; echo ; echo
-sleep 3;
-echo '>> Essential for WordPress permanent links (slugs) to work.';
+echo '>> Essential for WordPress permanent links (slugs) to work.'
 echo ; echo ; echo
-sleep 3;
-sudo a2enmod rewrite;
-sleep 3;
-echo ; echo ; echo
-echo '>> Just a moment. Now it going to restart Apache.';
-echo ; echo ; echo
-sleep 3;
-sudo service apache2 restart;
+sudo a2enmod rewrite
 echo ; echo ; echo
 
-echo '#################### Optimal settings in the php.ini file. ####################';
-sleep 3;
+echo '#################### Optimal settings in the php.ini file. ####################'
 echo ; echo ; echo
 wget https://github.com/CleoMenezes/LAMPP-Manager/blob/master/configuration%20files/000-default.conf
 sudo mv 000-default.conf /etc/apache2/sites-available/
 echo ; echo ; echo
-echo '#################### Lets install phpMyAdmin. ####################';
-sleep 3;
+echo '#################### Lets install phpMyAdmin. ####################'
 echo ; echo ; echo
-echo '>> Before we are going to install the following PHP plugins.';
+echo '>> Before we are going to install the following PHP plugins.'
 echo ; echo ; echo
-sleep 3;
-sudo apt install php-imagick php-phpseclib php-common php-imap php-zip php-xml php-mbstring php-bz2 -y;
+sudo apt install php-imagick php-phpseclib php-common php-imap php-zip php-xml php-mbstring php-bz2 -y
+
 echo ; echo ; echo
-echo '>> Just a moment. Now it going to restart Apache.';
+echo '>> Lets download the latest stable version of phpMyAdmin.'
 echo ; echo ; echo
-sleep 3;
-sudo service apache2 restart -y;
 echo ; echo ; echo
-echo '>> Lets download the latest stable version of phpMyAdmin.';
+sudo apt install unzip -y
 echo ; echo ; echo
-sleep 3;
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.zip
 echo ; echo ; echo
-sudo apt install unzip -y;
+unzip phpMyAdmin-4.9.7-all-languages.zip
 echo ; echo ; echo
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.zip;
+sudo mv phpMyAdmin-4.9.7-all-languages /usr/share/phpmyadmin
 echo ; echo ; echo
-unzip phpMyAdmin-4.9.7-all-languages.zip;
+sudo rm phpMyAdmin-4.9.7-all-languages.zip
 echo ; echo ; echo
-sudo mv phpMyAdmin-4.9.7-all-languages /usr/share/phpmyadmin;
+sudo chown -R www-data:www-data /usr/share/phpmyadmin
 echo ; echo ; echo
-sudo chown -R www-data:www-data /usr/share/phpmyadmin;
-echo ; echo ; echo
-echo -e '>> It is ideal that you create the phpMyAdmin database and assign it to our user as in the example below. You can do later:\n\nsudo mysql -u root -p\nCREATE DATABASE phpmyadmin;\nGRANT ALL PRIVILEGES ON phpmyadmin.* TO your_username;\n FLUSH PRIVILEGES;.';
-sleep 4;
+echo -e '>> It is ideal that you create the phpMyAdmin database and assign it to our user as in the example below. You can do later:\n\nsudo mysql -u root -p\nCREATE DATABASE phpmyadmin;\nGRANT ALL PRIVILEGES ON phpmyadmin.* TO your_username;\n FLUSH PRIVILEGES;.'
 echo ; echo ; echo
 wget https://github.com/CleoMenezes/LAMPP-Manager/blob/master/configuration%20files/phpmyadmin.conf
-sudo mv phpmyadmin.conf /etc/apache2/conf-available/;
+sudo mv phpmyadmin.conf /etc/apache2/conf-available/
 echo ; echo ; echo
-sleep 3;
-echo '>> Lets enable the snippet for the configuration';
+echo '>> Lets enable the snippet for the configuration'
 echo ; echo ; echo
-sleep 3;
 sudo a2enconf phpmyadmin.conf
 echo ; echo ; echo
-sleep 3;
-echo '>> Lets create the temporary directory of phpMyAdmin';
+echo '>> Lets create the temporary directory of phpMyAdmin'
 echo ; echo ; echo
-sleep 3;
 sudo mkdir -p /var/lib/phpmyadmin/tmp
 echo ; echo ; echo
-sleep 3;
-echo '>> Lets make the web server user (www-data) own the directory';
+echo '>> Lets make the web server user (www-data) own the directory'
 echo ; echo ; echo
-sleep 3;
-sudo chown www-data:www-data /var/lib/phpmyadmin/tmp;
+sudo chown www-data:www-data /var/lib/phpmyadmin/tmp
 echo ; echo ; echo
-sleep 3;
-echo '>> Just a moment. Now it going to restart Apache.';
-sleep 3;
-sudo service apache2 reload;
+echo '>> Check that phpMyAdmin is installed correctly.'
 echo ; echo ; echo
-echo '>> Check that phpMyAdmin is installed correctly.';
-echo ; echo ; echo
-sleep 3;
 sensible-browser http://localhost/phpmyadmin
 echo ; echo ; echo
-sleep 3;
+echo '#################### Lets install VSFTP. ####################'
 echo ; echo ; echo
-echo '#################### Lets install VSFTP. ####################';
-echo ; echo ; echo
-sleep 3;
 sudo apt-get install vsftpd
 echo ; echo ; echo
-sleep 3;
-echo '#################### Lets set permission to html folder. ####################';
-sudo chmod 777 -R /var/www/html;
+echo '#################### Lets set permission to html folder. ####################'
+sudo chmod 777 -R /var/www/html
 echo ; echo ; echo
 sleep 3;
 echo 'Enjoy!';
